728afe0f8012d05c92cbb516c55d10ece4ea296b
yuyusss <gch04713@naver.com>
2022-03-17 17:39:50 +0900

    3/17 yu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import { Link, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h2>index Page</h2>
+      <Link to='/login' ><button>로그인</button></Link> &nbsp;
+      <Link to='/signup' ><button>회원가입</button></Link> &nbsp;
+
+      <Routes>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<SignUp/>} />
+      </Routes>
     </div>
   );
 }
