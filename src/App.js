94e5e2e0aa3d182b7120e430eb5945281c212c5e
hyeonji0409 <guswl8507@naver.com>
2022-03-18 15:06:34 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route} from 'react-router-dom';
+import Portfolio from './components/Portfolio.js';
 
 function App() {
   return (
     <div className="App">
       <h2>index Page</h2>
+      <Route component={Portfolio} path='/Portfolio' />
     </div>
   );
 }
---
+++ src/components/Portfolio.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <button>Portfolio 등록</button>
+        </div>
+    )
+};
+
+export default Portfolio;
