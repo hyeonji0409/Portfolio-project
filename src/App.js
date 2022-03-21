aa576aac99cb01a8b9eb9ee26487eabbd3593817
yonghwi97 <hiw97@naver.com>
2022-03-21 17:33:20 +0900

    2022-03-21 17:33

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Career from './components/Career';
 
 function App() {
   return (
     <div className="App">
-      <h2>index Page</h2>
+      <Career></Career>
     </div>
   );
 }
