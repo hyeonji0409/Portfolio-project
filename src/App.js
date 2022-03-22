8b3d4c6a32153eb1d67d6fcd2c5910a4fa4a0f97
PastaEat <etti3540@gmail.com>
2022-03-23 02:38:07 +0900

    Bottom 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SignUp from './components/SignUp';
 import About from './components/About';
 import Introduce from './components/Introduce';
 import { Link, Routes, Route } from 'react-router-dom';
+import Bottom from './components/Bottom';
 
 import Mypage from './components/Mypage';
 
@@ -12,27 +13,34 @@ import Portfolio from './components/Portfolio';
 
 function App() {
   return (
-    <div className="App">
-      <div className='MenuBar'>
-        <Link to='/' ><button className='MenuButton'>home</button></Link> &nbsp;
-        <Link to='/introduce' ><button className='MenuButton'>introduce</button></Link> &nbsp;
-        <Link to='/login' ><button className='MenuButton'>login</button></Link> &nbsp;
-        <Link to='/signup' ><button className='MenuButton'>signup</button></Link> &nbsp;
-        <Link to='/mypage' ><button className='MenuButton'>마이페이지</button></Link> &nbsp;
-        <Link to='/logout' ><button className='MenuButton'>로그아웃</button></Link> &nbsp;
+    <>
+      <div className="App">
+        <div className='MenuBar'>
+          <Link to='/' ><button className='MenuButton'>home</button></Link> &nbsp;
+          <Link to='/introduce' ><button className='MenuButton'>introduce</button></Link> &nbsp;
+          <Link to='/login' ><button className='MenuButton'>login</button></Link> &nbsp;
+          <Link to='/signup' ><button className='MenuButton'>signup</button></Link> &nbsp;
+          <Link to='/mypage' ><button className='MenuButton'>마이페이지</button></Link> &nbsp;
+          <Link to='/logout' ><button className='MenuButton'>로그아웃</button></Link> &nbsp;
 
-        {/* <hr></hr> */}
+          {/* <hr></hr> */}
+        </div>
+
+
+
+        <Routes>
+          <Route path='/' element={<About />} />
+          <Route path='/Introduce' element={<Introduce />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/mypage' element={<Mypage />} />
+          <Route path='/logout' element={<Mypage />} />
+        </Routes>
       </div>
-
-      <Routes>
-        <Route path='/' element={<About />} />
-        <Route path='/Introduce' element={<Introduce />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/mypage' element={<Mypage />} />
-        <Route path='/logout' element={<Mypage />} />
-      </Routes>
-    </div>
+      <div>
+        <Bottom />
+      </div>
+    </>
   );
 }
 
