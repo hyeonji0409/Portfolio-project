5d4ba704ee111e06292945c574f1a8b60511d457
PastaEat <etti3540@gmail.com>
2022-03-23 16:14:20 +0900

    3-23/16시

--- src/components/PortfolioInsert.js
+++ src/components/PortfolioInsert.js
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import React , { useState, Component }from "react";
+import React, { useState, Component } from "react";
 import axios from 'axios';
 import './PortfolioInsert.scss';
 import TextField from '@mui/material/TextField';
@@ -25,19 +25,19 @@ function PortfolioInsert(props) {
         const { value, name } = e.target; // e.target에서 name과 value 추출
         setPort({
             ...port, // 기존의 port객체 복사
-            [name] : value // name 키를 가진 값을 value로 설정
+            [name]: value // name 키를 가진 값을 value로 설정
         });
     };
 
     const onReset = () => {
         setPort({
             memId: '',
-        portStackNo: '',
-        portNo: '',
-        portTitle: '',
-        portSubTitle: '',
-        gitLink: '',
-        portDetails: ''
+            portStackNo: '',
+            portNo: '',
+            portTitle: '',
+            portSubTitle: '',
+            gitLink: '',
+            portDetails: ''
         })
     };
 
@@ -47,95 +47,95 @@ function PortfolioInsert(props) {
         var frmData = new FormData(document.portfolioInsert);
 
         axios.post('http://localhost:8080/insertPortfolio/' + frmData)
-        .then(
-            response => {
-                alert("등록 완료");
-                history('portfolio'); // portfolio 페이지로 이동
-            }
-        );
+            .then(
+                response => {
+                    alert("등록 완료");
+                    history('portfolio'); // portfolio 페이지로 이동
+                }
+            );
     }
-    
-  return (
-    <div className='container_portfolioInsert'>
-        <div className='insertForm'>
-            <h2>Insert Your Project</h2>
+
+    return (
+        <div className='container_portfolioInsert'>
+            <div className='insertForm'>
+                <h2>Insert Your Project</h2>
+            </div>
+
+            <form className='portfolioInsert' name='portfolioInsert' onSubmit={onSubmit} onReset={onReset}>
+                <table className='poTable'>
+                    <tr>
+                        <th>
+                            제목
+                        </th>
+                        <td>
+                            <input type='text' name="portTitle" id="portTitle"
+                                value={port.portTitle} onChange={onChange} maxLength="30" placeholder='제목을 입력하세요'></input>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th>
+                            프로젝트 설명
+                        </th>
+                        <td>
+                            <input type='text' name="portSubTitle" id="portSubTitle"
+                                value={port.portSubTitle} onChange={onChange} maxLength="100" placeholder='프로젝트에 대해 간단히 설명해주세요'></input>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th>
+                            기술 스택
+                        </th>
+                        <td>
+                            <Autocomplete
+                                disablePortal
+                                id="combo-box-demo"
+                                options={stacks}
+                                sx={{ width: 300 }}
+                                renderInput={(params) => <TextField {...params} label="Stack" />}
+                            />
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th>
+                            Github 주소
+                        </th>
+                        <td>
+                            <input type='text' name="gitLink" id="gitLink"
+                                value={port.gitLink} onChange={onChange} placeholder='Github 링크를 첨부해주세요'></input>
+                        </td>
+                    </tr>
+                </table>
+                <div className='editor'>
+                    <CKEditor
+                        editor={ClassicEditor}
+                        data="<p>Hello from CKEditor 5!</p>"
+                        onReady={editor => {
+                            // You can store the "editor" and use when it is needed.
+                            console.log('Editor is ready to use!', editor);
+                        }}
+                        onChange={(event, editor) => {
+                            const data = editor.getData();
+                            console.log({ event, editor, data });
+                        }}
+                        onBlur={(event, editor) => {
+                            console.log('Blur.', editor);
+                        }}
+                        onFocus={(event, editor) => {
+                            console.log('Focus.', editor);
+                        }}
+                        value={port.portDetails}
+                    />
+                </div>
+
+                <input class="portBtn" type='reset' value="취소" />
+                <input class="portBtn" type='submit' value="등록" />
+
+            </form>
         </div>
-
-        <form className='portfolioInsert' name='portfolioInsert' onSubmit={onSubmit} onReset={onReset}>
-            <table className='poTable'>
-                <tr>
-                    <th>
-                        제목
-                    </th>
-                    <td>
-                         <input type='text' name="portTitle" id="portTitle" 
-                         value={port.portTitle} onChange={onChange} maxLength="30" placeholder='제목을 입력하세요'></input>
-                    </td>
-                </tr>
-
-                <tr>
-                    <th>
-                        프로젝트 설명
-                    </th>
-                    <td>
-                        <input type='text' name="portSubTitle" id="portSubTitle" 
-                        value={port.portSubTitle} onChange={onChange} maxLength="100" placeholder='프로젝트에 대해 간단히 설명해주세요'></input>
-                    </td>
-                </tr>
-
-                <tr>
-                    <th>
-                        기술 스택
-                    </th>
-                    <td>
-                        <Autocomplete
-                            disablePortal
-                            id="combo-box-demo"
-                            options={stacks}
-                            sx={{ width: 300 }}
-                            renderInput={(params) => <TextField {...params} label="Stack" />}
-                        />
-                    </td>
-                </tr>
-
-                <tr>
-                    <th>
-                        Github 주소
-                    </th>
-                    <td>
-                        <input type='text' name="gitLink" id="gitLink" 
-                        value={port.gitLink} onChange={onChange} placeholder='Github 링크를 첨부해주세요'></input>
-                    </td>
-                </tr>
-            </table>
-            <div className='editor'>
-                <CKEditor
-                    editor={ ClassicEditor }
-                    data="<p>Hello from CKEditor 5!</p>"
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                    value={port.portDetails}
-                />
-            </div>		
-            	
-            <input class="portBtn" type='reset' value="취소"/>
-            <input class="portBtn" type='submit' value="등록"/>
-      			
-        </form>
-    </div>
-  )
+    )
 }
 
 const stacks = [
@@ -146,6 +146,6 @@ const stacks = [
     { label: 'Javascript', year: 1957 },
     { label: "jQuery", year: 1993 },
     { label: 'Git', year: 1994 },
-  ];
+];
 
 export default PortfolioInsert
